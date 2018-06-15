48d1f79577380d422a9aa38abe7ccc036a8306af
vikkymahdrey <vikky.softengi@gmail.com>
2018-06-15 13:27:15 +0530

    initial commit
    initial commit

---
+++ TestingAngular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './comp/login/login.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+    
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
